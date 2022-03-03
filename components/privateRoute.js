0e35498285e6922aaee7dba63baa2a2e3dbc4b35
Mathias <mat.micheli99@gmail.com>
2022-03-03 11:25:10 +0100

    feat: handle refresh token

--- components/privateRoute.js
+++ components/privateRoute.js
@@ -3,12 +3,31 @@ import React, { Component } from "react";
 import { AuthToken } from "../services/auth_token";
 
 export function privateRoute(WrappedComponent) {
-
     return class extends Component {
-
         static async getInitialProps(ctx) {
             const token = nookies.get(ctx)['token'];
-            const auth = new AuthToken(token);
+            let auth = new AuthToken(token);
+            const refresh = nookies.get(ctx)['refresh'];
+            if(auth.isExpired && refresh) {
+                let refreshedToken = null;
+                await fetch(
+                    'http://localhost:8010/proxy/api/token/refresh',
+                    {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            refresh_token: refresh
+                        }),
+                        headers: {
+                            'Content-type': 'application/json'
+                        }
+                    }
+                )
+                    .then(response => response.json())
+                    .then(response => {
+                        refreshedToken = response.token
+                    })
+                auth = new AuthToken(refreshedToken);
+            }
             const initialProps = { auth };
             if (auth.isExpired) {
                 ctx.res.writeHead(302, {
