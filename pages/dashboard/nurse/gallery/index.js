5e348ad4e9d47a460b1aedd99f303042d2470714
Mathias <mat.micheli99@gmail.com>
2022-04-05 18:41:40 +0200

    feat: add delete image

--- pages/dashboard/nurse/gallery/index.js
+++ pages/dashboard/nurse/gallery/index.js
@@ -3,39 +3,97 @@ import {AuthToken} from "../../../../services/auth_token";
 import {getSession} from "next-auth/react";
 import Gallery from "react-photo-gallery";
 import Carousel, {Modal, ModalGateway} from "react-images";
+import {ActionIcon, Button, Divider, Group, Text} from "@mantine/core";
+import {AiOutlineClose, AiTwotoneDelete} from "react-icons/ai";
+import {useRouter} from "next/router";
 
 function AddGallery({bearer, userId, galleryPhoto}) {
 
-    console.log(galleryPhoto)
     const [currentImage, setCurrentImage] = useState(0);
     const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const router = useRouter();
+
     const openLightbox = useCallback((event, {photo, index}) => {
         setCurrentImage(index);
         setViewerIsOpen(true);
     }, []);
+
     const closeLightbox = () => {
         setCurrentImage(0);
         setViewerIsOpen(false);
     };
+
+    const deleteImage = async () => {
+        const res = await fetch(`http://localhost:8010/proxy/api/gallery/${galleryPhoto[currentImage].id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': bearer
+            }
+        })
+
+        if (res.status === 204) {
+            router.reload();
+        }
+    }
+
+    const CustomHeader = ({innerProps, isModal}) => isModal ? (
+        <div
+            style={{
+                marginTop: 100,
+                marginLeft: 50,
+            }}
+        >
+            <Group>
+                <ActionIcon
+                    onClick={deleteImage}
+                    color="gray"
+                    size="xl"
+                    radius="xs"
+                    variant="filled">
+                    <AiTwotoneDelete size={25}/>
+                </ActionIcon>
+                <Divider size="xl" orientation={"vertical"} variant={"solid"}/>
+                <ActionIcon
+                    onClick={closeLightbox}
+                    color="gray"
+                    size="xl"
+                    radius="xs"
+                    variant="filled">
+                    <AiOutlineClose size={25}/>
+                </ActionIcon>
+            </Group>
+        </div>
+    ) : null;
+
     return (
         <div>
-            <Gallery photos={galleryPhoto} onClick={openLightbox}/>
-            <ModalGateway>
-                {viewerIsOpen ?
-                    (
-                        <Modal onClose={closeLightbox}>
-                            <Carousel
-                                currentIndex={currentImage}
-                                views={galleryPhoto.map(x => (
-                                    {
-                                        ...x,
-                                        srcset: x.srcSet,
-                                        caption: x.title
-                                    })
-                                )}
-                            />
-                        </Modal>) : null}
-            </ModalGateway>
+            {
+                galleryPhoto.length === 0 ?
+                    <Text>Aucunes photos d'enregistrées</Text>
+                    :
+                    <>
+                        <Gallery photos={galleryPhoto} onClick={openLightbox}/>
+                        <ModalGateway>
+                            {viewerIsOpen ?
+                                (
+                                    <Modal onClose={closeLightbox}>
+                                        <Carousel
+                                            components={{Header: CustomHeader}}
+                                            currentIndex={currentImage}
+                                            views={galleryPhoto.map(x => (
+                                                {
+                                                    ...x,
+                                                    srcset: x.srcSet,
+                                                    caption: x.title
+                                                })
+                                            )}/>
+                                    </Modal>
+                                ) : null}
+                        </ModalGateway>
+                    </>
+            }
+
         </div>
     )
 }
@@ -50,7 +108,10 @@ export async function getServerSideProps(ctx) {
 
     const res = await fetch(`http://localhost:8010/proxy/api/gallery/nurse/${authToken.decodedToken.id}`, {
         method: 'GET',
-        headers: {'Content-type': 'application/json', 'Authorization': authToken.authorizationString}
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': authToken.authorizationString
+        }
     });
 
     const photos = await res.json();
@@ -60,10 +121,19 @@ export async function getServerSideProps(ctx) {
         galleryPhoto = photos.map(
             (element) => (
                 {
+                    id: element.id,
                     src: "http://127.0.0.1:8887/" + authToken.decodedToken.id + '/' + element.url,
-                    width: 3,
-                    height: 3}
+                    width: 4,
+                    height: 3,
+                }
             ))
     }
-    return {props: {userId: sessionCallBack.user.id, bearer: authToken.authorizationString, galleryPhoto}}
+    return {
+        props:
+            {
+                userId: sessionCallBack.user.id,
+                bearer: authToken.authorizationString,
+                galleryPhoto
+            }
+    }
 }
