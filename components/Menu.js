d0e20a5efbbec9cb6444008d24d3904f83bdedb2
Mathias <mat.micheli99@gmail.com>
2022-04-13 15:36:31 +0200

    fix: format menu date

--- components/Menu.js
+++ components/Menu.js
@@ -1,24 +1,25 @@
 import {Button, Space, Text, Title} from "@mantine/core";
 import Link from 'next/link';
+import dayjs from "dayjs";
+import 'dayjs/locale/fr';
 
 function Menu({menu, role}) {
-    console.log(menu)
     return (
         <div>
             { menu ?
                 <>
-                    <Title>Menu du {menu.date}</Title>
+                    <Title order={1}>Menu du {dayjs(menu.date).add(1, 'day').locale('fr').format('DD MMMM YYYY')}</Title>
                     <Space h={'xl'}/>
 
-                    <Title>Entrée</Title>
+                    <Title order={2}>Entrée</Title>
                     <Text>{menu.entry}</Text>
                     <Space h={'xl'}/>
 
-                    <Title>Plat</Title>
+                    <Title order={2}>Plat</Title>
                     <Text>{menu.meal}</Text>
                     <Space h={'xl'}/>
 
-                    <Title>Dessert</Title>
+                    <Title order={2}>Dessert</Title>
                     <Text>{menu.dessert}</Text>
                 </> :
                 <>
