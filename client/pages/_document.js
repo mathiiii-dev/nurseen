5d2ae172b601d351baba185d74d56e44d4c8dddd
Mathias <mat.micheli99@gmail.com>
2022-06-12 17:27:31 +0200

    fix: broken css

---
+++ client/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>
+                    Nurseen
+                </title>
+                <link rel='stylesheet' href='../styles/globals.css' />
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    )
+}
